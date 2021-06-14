cc947d67ced28ee99b47923c260fba851f565140
mrmamedzade <cafarmm@code.edu.az>
2021-06-14 16:37:47 +0400

    Register error settings are done.

--- BackendProject/BackendProject/Controllers/AccountController.cs
+++ BackendProject/BackendProject/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BackendProject.Models;
+using BackendProject.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,13 @@ namespace BackendProject.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -15,6 +25,42 @@ namespace BackendProject.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var dbUser = await _userManager.FindByNameAsync(register.Username);
+            if(dbUser != null)
+            {
+                ModelState.AddModelError("Username", "Bu adda user var!");
+                return View();
+            }
+
+            var newUser = new User
+            {
+                UserName = register.Username,
+                Fullname = register.Fullname,
+                Email = register.Email
+            };
+
+            var identityResult=await _userManager.CreateAsync(newUser, register.Password);
+            if (!identityResult.Succeeded)
+            {
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    return View();
+                }
+            }
+
+            return RedirectToAction("Login");
         }
     }
 }
